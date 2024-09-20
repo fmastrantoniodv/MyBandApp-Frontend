0ed449397f4eb1301d3e623e063ff7a130f8cbf0
Frankie <franco.mastrantonio@davinci.edu.ar>
2024-09-19 21:32:39 -0300

    ajustes responsive

--- my-react-app/src/components/Studio/VolumeController.js
+++ my-react-app/src/components/Studio/VolumeController.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import '../../App.css';
 
 function VolumeController({sample}) {
     const [volumeValue, setVolumeValue] = useState(sample.channelConfig.volume);
